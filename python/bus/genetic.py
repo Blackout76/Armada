1b741cdc214fb49f59d04e9858c9293d0194eb2c
Blackout <olivier.leger@imerir.com>
2015-12-01 11:48:07 +0100

    compute basic population score

--- python/bus/genetic.py
+++ python/bus/genetic.py
@@ -7,7 +7,7 @@ from math import *
 
 
 score_objectif = 23333.0000001
-population_size = 1
+population_size = 20
 iteration = 1
 child_population_size = 75
 parents_count_min = 2
@@ -15,20 +15,20 @@ parents_count_max = 2
 selection_type = 'roulette' # alea / roulette 
 adn_croisement_count = 1
 
-travels = generateTravels()
 links = generateLiaisons()
-
-for l in links:
-	print l
-print l
-
+travels = generateTravels(links)
 
 timeStart = time()
 adnBase = generateBasicADN(travels)
-nbBus = len(adnBase) / 2
+nbBus = 100
 population = createPopulation(population_size, adnBase,nbBus)
+evalPopulation(travels,population,links)
+for individu in population:
+	print individu.score
 
 
+
+"""
 bus = []
 for i in range(nbBus+1):
 	bus.append(0)
@@ -39,7 +39,7 @@ for j in range(len(population)):
 
 for i in range(len(bus)):
 	print 'Bus ' + str(i) + ' = ' + str(bus[i]*1.0/population_size)
-
+"""
 
 
 # print'###################'
@@ -57,7 +57,7 @@ print '		population size: ' + str(population_size)
 print '		population children size: ' + str(child_population_size)
 print '		selection: ' + selection_type
 print '		count of croisement ADN: ' + str(adn_croisement_count)
-print '		executed time: ' + str(round((time() - timeStart)*100)/100) + 'ms in ' + str(iteration) + ' generation'
+print '		executed time: ' + str(round((time() - timeStart)*100)/100) + 's in ' + str(iteration) + ' generation'
 print 'Score:'
 print '		min: ' + str(population[population_size-1].score) 
 print '		max: ' + str(population[0].score) 
