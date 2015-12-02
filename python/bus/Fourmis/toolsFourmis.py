e9b81018b5a93ffa6cca8af3c18f4e1d44d1ca7d
Jeremy-Imerir <jeremy.jouet@imerir.com>
2015-12-02 16:05:27 +0100

    ajout des voisins possible

--- python/bus/Fourmis/toolsFourmis.py
+++ python/bus/Fourmis/toolsFourmis.py
@@ -1,23 +1,88 @@
 import copy
 
+# def updateList(liste1, liste2):
+# 	# la liste 1 a maj a partir de la liste 2 .... liste1 - liste2 si meme valeur
+# 	copyL1 = copy.deepcopy(liste1)
+# 	copyL2 = copy.deepcopy(liste2)
+
+# 	for i in range(len(copyL2)):
+# 		for j in range(len(copyL1)):
+# 			if copyL2[i] == copyL1[j]:
+# 				del copyL1[j]
+# 				break
+# 	return copyL1	
+
 def updateList(liste1, liste2):
 	# la liste 1 a maj a partir de la liste 2 .... liste1 - liste2 si meme valeur
-	copyL1 = copy.deepcopy(liste1)
-	copyL2 = copy.deepcopy(liste2)
+	copyL1 = liste1
+	copyL2 = liste2
 
 	for i in range(len(copyL2)):
 		for j in range(len(copyL1)):
 			if copyL2[i] == copyL1[j]:
 				del copyL1[j]
 				break
-	return copyL1	
 
-def depotph():
-	ph = 0
-	#recuper la distance et on l'inverse : 1/D , ce qui correspond a beaucoup de pheromone pour un petit trajet.
-	return ph
+def listeChrono(liste):
+	copyListe = liste
+	tailleL = len(copyListe)
+	listeChro = []
 
+	for i in xrange(0,tailleL):
 
+		tailleCopy = len(copyListe)
+		heureMin= 0
+		
+		for j in xrange(0,tailleCopy):
+			if copyListe[heureMin].startPoint.time.inMin() > copyListe[j].startPoint.time.inMin():
+				heureMin = j
+				pass
+			pass
+		listeChro.append(copyListe[heureMin])
+		updateList(copyListe,listeChro)
+		pass
+
+	# for x in xrange(0,len(listeChro)):
+	# 	print "Trajet" + str(x) + " " + str(listeChro[x].startPoint.time.hour) + ":" + str(listeChro[x].startPoint.time.min)
+	# 	pass
+
+	return listeChro
+
+def voisinsPossible(liste, point):
+	copyListe = liste
+	pointActuel = copy.deepcopy(point)
+	pointSuivant = copy.deepcopy(pointActuel) + 1
+
+	listeVoisins =  []
+
+	if pointActuel == 0:
+		listeVoisins.append(liste[pointActuel])
+
+		while copyListe[pointActuel].endPoint.time.inMin() > (copyListe[pointSuivant].startPoint.time.inMin() + 5):
+			listeVoisins.append(liste[pointSuivant])
+			pointSuivant = pointSuivant + 1
+			pass
+		pass
+
+	else:
+		while copyListe[pointActuel].endPoint.time.inMin() > (copyListe[pointSuivant].startPoint.time.inMin() + 5):
+			pointSuivant = pointSuivant + 1
+			pass
+
+		listeVoisins.append(liste[pointSuivant])
+		pointVoisin = copy.deepcopy(pointSuivant) + 1
+
+		while copyListe[pointSuivant].endPoint.time.inMin() > (copyListe[pointVoisin].startPoint.time.inMin() + 5):
+			listeVoisins.append(liste[pointVoisin])
+			pointVoisin = pointVoisin + 1
+			pass
+		pass
+
+	# for x in xrange(0,len(listeVoisins)):
+	# 	print "Trajet" + str(x) + " " + str(listeVoisins[x].startPoint.time.hour) + ":" + str(listeVoisins[x].startPoint.time.min) + " " + str(listeVoisins[x].endPoint.time.hour) + ":" + str(listeVoisins[x].endPoint.time.min)
+	# 	pass
+
+	return listeVoisins
 
 
 # def chooseTravel(pointActuel, listeVoisins):
@@ -43,3 +108,13 @@ def depotph():
 # 			choixVoisin = i
 
 # 	return choixVoisin
+
+
+
+
+
+
+
+
+
+
--- python/bus/fourmis.py
+++ python/bus/fourmis.py
@@ -11,45 +11,39 @@ from Fourmis.toolsFourmis import *
 links = generateLiaisons()
 travels = generateTravels(links)
 
-listeTrajets=[1,2,3,4,5,6,7,8,9]
-print listeTrajets
-
-listeTrajetEffectuer=[3,5,2,7]
-print listeTrajetEffectuer
-
-majTrajets = copy.deepcopy(listeTrajets)
-
-majTrajets = updateList(majTrajets, listeTrajetEffectuer)
-
-print majTrajets
-
-
 ##############################################
 #####             Debut ALGO	         #####
 ##############################################
 
-ListFake = [1,2,3]
-ListTotal = copy.deepcopy(ListFake)
-nbIteration = 2
-nbFourmis = 50
+# ListFake = [1,2,3]
+# ListTotal = copy.deepcopy(ListFake)
+# nbIteration = 2
+# nbFourmis = 50
 
-while len(ListTotal)!=0:
-	for iteration in xrange(0,nbIteration):
-		for fourmis in xrange(0, nbFourmis):
-			tabTF = []
-			copyLT = copy.deepcopy(ListTotal)
-			positionActuel = 0
-			while len(copyLT)!=0:
-				#fonction choix du chemin a faire, simulation :
-				tabTF.append(copyLT[positionActuel])
-				print tabTF
-				copyLT = updateList(copyLT,tabTF)
-				pass
-			#print "iteration: " + str(iteration) + " fourmis: " + str(fourmis)
-			pass
-		pass
-	ListTotal = updateList(ListTotal,tabTF)
-	pass
+# while len(ListTotal)!=0:
+# 	for iteration in xrange(0,nbIteration):
+# 		for fourmis in xrange(0, nbFourmis):
+# 			tabTF = []
+# 			copyLT = copy.deepcopy(ListTotal)
+# 			positionActuel = 0
+# 			while len(copyLT)!=0:
+# 				#fonction choix du chemin a faire, simulation :
+# 				tabTF.append(copyLT[positionActuel])
+# 				print tabTF
+# 				copyLT = updateList(copyLT,tabTF)
+# 				pass
+# 			#print "iteration: " + str(iteration) + " fourmis: " + str(fourmis)
+# 			pass
+# 		pass
+# 	ListTotal = updateList(ListTotal,tabTF)
+# 	pass
 
-print "liste total " 
-print ListTotal
+# print "liste total " 
+# print ListTotal
+
+travels2 = generateTravels(links)
+print len(travels2)
+travels3 = listeChrono(travels2)
+print len(travels3)
+#print "pointActuel Heure fin: " + str(travels3[0].endPoint.time.inMin())
+voisinsPossible(travels3, 0)
