e58dfbd9d41220c1b9a51cb48229bf3df491288e
Jeremy-Imerir <jeremy.jouet@imerir.com>
2015-12-03 12:15:03 +0100

    maj fourmis olive

--- python/bus/Fourmis/toolsFourmis.py
+++ python/bus/Fourmis/toolsFourmis.py
@@ -79,11 +79,18 @@ def voisinsPossible(liste, point):
 		while copyListe[pointSuivant].endPoint.time.inMin() > (copyListe[pointVoisin].startPoint.time.inMin() + 5) and sortir == 0:
 			listeVoisins.append(liste[pointVoisin])
 			pointVoisin = pointVoisin + 1
+
+
+
+			print pointVoisin
+
+
+
 			if pointVoisin==538 :
 				sortir = 1
 
-	# for x in xrange(0,len(listeVoisins)):
-	# 	print "Trajet" + str(x) + " " + str(listeVoisins[x].startPoint.time.hour) + ":" + str(listeVoisins[x].startPoint.time.min) + " " + str(listeVoisins[x].endPoint.time.hour) + ":" + str(listeVoisins[x].endPoint.time.min)
+	for x in xrange(0,len(listeVoisins)):
+	 	print "Trajet" + str(x) + " " + str(listeVoisins[x].startPoint.time.hour) + ":" + str(listeVoisins[x].startPoint.time.min) + " " + str(listeVoisins[x].endPoint.time.hour) + ":" + str(listeVoisins[x].endPoint.time.min)
 	 
 
 	return listeVoisins
@@ -142,10 +149,82 @@ def pheromone(liste):
 
 
 
+def move(travelStart, path, links):
+	
+	path.append(travelStart)
+	voisins = travelStart.travelAfter
+	voisins.sort(key = lambda x: x.startPoint.time.inMin(), reverse = False)
+	realisable = []
+	if len(voisins)>0:
+		for v in voisins[0].travelUncompatible:
+			if v.startPoint.time.inMin() >= voisins[0].startPoint.time.inMin():
+				realisable.append(v)
+
+	if len(realisable)>0:
+		return move(choiceNextTravel(travelStart,realisable,links), path,links)
+	else:
+		return path
+
+def choiceNextTravel(travelStart,travels,links):
+	travel = travels[randint(0, len(travels)-1)]
+
+	pheromone=[0 for x in travels]
+ 	visibilite=[0 for x in travels]
+ 	probChoisir=[0 for x in travels]
+ 	total = 0
 
 
+ 	for i in range(len(travels)):
+ 		pheromone[i] = travels[i].pheromones
+		link = links.get(str(travelStart.endPoint.name + ':' + travels[i].startPoint.name))
+ 		visibilite[i] = 1.0 / (int(link.dist) + (((int(link.time)+5)/60.0)*25.0))
+ 		total += pheromone[i] * visibilite[i]
 
+	probMax = 0
+ 	nextTravel = 0
+ 	if total == 0:
+ 		for i in range(len(visibilite)):
+	 		if visibilite[i] > probMax:
+	 			probMax=visibilite[i]
+	 			nextTravel = travels[i]
+	else:
+		for i in range(len(probChoisir)):
+	 		probChoisir[i] = (pheromone[i] * visibilite[i]) / total 
+	 		if probChoisir[i] > probMax:
+	 			probMax=probChoisir[i]
+	 			nextTravel = travels[i]
 
+ 	return nextTravel
+
+def computePath(path,links):
+	scoreDist = 0
+	scoreTime = 0
+	lastTravelEndPointName = ''
+	for travelIndex in range(len(path)):
+		scoreDist += int(path[travelIndex].dist)
+		scoreTime += int(path[travelIndex].endPoint.time.inMin()) - int(path[travelIndex].startPoint.time.inMin())
+		if travelIndex == 0:
+			#print links.get(str('T0' + ':' + path[travelIndex].startPoint.name)).dist
+			scoreDist += int(links.get(str('T0' + ':' + path[travelIndex].startPoint.name)).dist)
+			scoreTime += int(links.get(str('T0' + ':' + path[travelIndex].startPoint.name)).time)
+			lastTravelEndPointName = path[travelIndex].endPoint.name
+		else:
+			scoreDist += int(links.get(str(lastTravelEndPointName + ':' + path[travelIndex].startPoint.name)).dist)
+			scoreTime += int(links.get(str(lastTravelEndPointName + ':' + path[travelIndex].startPoint.name)).time) + 5
+			lastTravelEndPointName = path[travelIndex].endPoint.name
+
+		if travelIndex == len(path)-1:
+			scoreDist += int(links.get(str(lastTravelEndPointName + ':' + 'T0')).dist)
+			scoreTime += int(links.get(str(lastTravelEndPointName + ':' + 'T0')).time) + 5
+	return [scoreTime,scoreDist]
+
+def putPheromones(path,score):
+	for p in path:
+		p.pheromones += 1 / (score[1] + ((score[0]/60.0)*25))
+
+def evapore(travels):
+	for t in travels:
+		t.pheromones *= 0.5
 
 
 
--- python/bus/fourmis.py
+++ python/bus/fourmis.py
@@ -8,58 +8,80 @@ from Fourmis.toolsFourmis import *
 
 ###########################################
 
-links = generateLiaisons()
-travels = generateTravels(links)
+#links = generateLiaisons()
+#travels = generateTravels(links)
 
 ##############################################
 #####             Debut ALGO	         #####
 ##############################################
 
-nbIteration = 2
-nbFourmis = 50
-
-
-travels2 = generateTravels(links)
+nbIteration = 10
+nbFourmis = 10
+links = generateLiaisons()
+print "links generer"
+travels = generateTravels(links)
 print "travels generer"
-travels3 = listeChrono(travels2)
+travels2 = listeChrono(travels)
 print "travels par ordre chrono"
+
+for j in range(nbIteration):
+	for i in range(nbFourmis):
+		print '-------'
+		path =[]
+		premierVoyage = travels2[0].travelUncompatible
+
+		if (premierVoyage != 0):
+			choix = randint(0, len(premierVoyage)-1)
+			path = move(travels2[choix], [travels2[0]],links)
+		else:
+			path = move(travels2[0], [travels2[0]],links)
+
+		score = computePath(path,links)
+		putPheromones(path,score)
+		for t in path:
+			print t.toString() + '   pheromones:' + str(t.pheromones)
+	evapore(travels)
+
+
+
+
 
 
 # while len(travels3)!=0:
 
-for iteration in xrange(0,nbIteration):
+# for iteration in xrange(0,nbIteration):
 
-	travelIteration = generateTravels(links)
-	print "travels total iteration generer"
-	travelIterationIteration = listeChrono(travelIteration)
-	print "travels total iteration chrono generer"
+# 	travelIteration = generateTravels(links)
+# 	print "travels total iteration generer"
+# 	travelIterationIteration = listeChrono(travelIteration)
+# 	print "travels total iteration chrono generer"
 
-	for fourmis in xrange(0, nbFourmis):
+# 	for fourmis in xrange(0, nbFourmis):
 
-		Tk = []
-		travelTotal = generateTravels(links)
-		print "travels total fourmis generer"
-		travelFourmis = listeChrono(travelTotal)
-		print "travels fourmis generer"
+# 		Tk = []
+# 		travelTotal = generateTravels(links)
+# 		print "travels total fourmis generer"
+# 		travelFourmis = listeChrono(travelTotal)
+# 		print "travels fourmis generer"
 
-		choix = 0
-		Tk.append(travelIterationIteration[choix])
+# 		choix = 0
+# 		Tk.append(travelIterationIteration[choix])
 
-		while len(travelFourmis) != 0:
-			choixPrecedent = choix
+# 		while len(travelFourmis) != 0:
+# 			choixPrecedent = choix
 
-			voisins = voisinsPossible(travelIterationIteration, choix)
-			choix = choix + choisirVoisin(voisins)
-			Tk.append(travelIterationIteration[choix])
+# 			voisins = voisinsPossible(travelIterationIteration, choix)
+# 			choix = choix + choisirVoisin(voisins)
+# 			Tk.append(travelIterationIteration[choix])
 
-			supprFourmis = choix-choixPrecedent
+# 			supprFourmis = choix-choixPrecedent
 
-			for x in xrange(0,(supprFourmis)):
-				del travelFourmis[x]
-				pass
-			len(travelFourmis)
+# 			for x in xrange(0,(supprFourmis)):
+# 				del travelFourmis[x]
+# 				pass
+# 			len(travelFourmis)
 
-			pass
+# 			pass
 		
-		pass
-	pass
+# 		pass
+# 	pass
